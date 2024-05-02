752cb4a15cbe903aabf706bf786fb315cc5c4f8c
Noam Schuck <noam.schuck@gmail.com>
2024-05-02 16:20:31 -0400

    working border sensor (took out interrupt stuff for now)

--- main.c
+++ main.c
@@ -1,60 +1,42 @@
 // ATMEGA328P Datasheet: https://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-7810-Automotive-Microcontrollers-ATmega328P_Datasheet.pdf
-
+ 
 /* Pins:
- *      Port D    - Output
- *      Port D, 1 - Passed threshold indicator
- *      Port C, 0 - IR Sensor
- *      Port C, 1 - Potentiometer
- */
-
+*      Port D    - Output
+*      Port D, 1 - Passed threshold indicator
+*      Port C, 0 - IR Sensor
+*      Port C, 1 - Potentiometer
+*/
+ 
 #include <xc.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
-
+ 
 void setup_ADC(void);
-void setup_interrupt(void);
 unsigned int get_ADC(unsigned char);
 void change_bit(volatile uint8_t *, unsigned char, unsigned char);
-
+ 
 unsigned int threshold = 0;
-
+unsigned int result = 0;
+ 
 int main(void)
-{
+{   
     DDRD = 0b11111111;     // Sets all bits in Port D to be outputs
-    
-    setup_ADC();
-    threshold = get_ADC(1); // Sets the treshold to the potentiometer value
-    
-    setup_interrupt();
-    
+    setup_ADC();    
     // What happens from now till infinity
     while(1) {
-        threshold = getADC(1);
+        threshold = get_ADC(1);                                            // gets result of pot threshold
+		result = get_ADC(0);                                               // Get the result of the IR ADC
+		PORTD = (result >= threshold) ? (PORTD | 0x01) : (PORTD & ~0x01);  // Outputs to port d bit 1
     }
 }
-
-// Interupt service routine for timer 0 
-ISR(TIMER0_OVF_vect) 
-{
-    unsigned int result = get_ADC(0);                  // Get the result of the IR ADC
-    TCNT0 = 0;                                         // Reset the timer count to 0
-    PORTD &= (result >= threshold) ? 0xFE : 0xFF;      // Set PD1 to 1 if result >= threshold
-}
-
-void setup_interrupt(void) {
-    TCCR0B |= (1 << CS02);  // Select 1024 clock cycles for the interupt
-    TIMSK0 |= (1 << TOIE0); // Enables the timer interupt 
-    sei();                  // Enables general interupt
-    TCNT0 = 0;              // Sets the timer counter to 0 (i think)
-}
-
+ 
 // Sets up ADC
 void setup_ADC(void)
 {
     ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0); // Enable ADC | Set Prescaler Bits (0-2) to 111 (128 ticks of system clock is 1 tick for the ADC (i think))
     ADMUX = (1<<REFS0);                                  // Set voltage reference to AVcc with external cap at AREF pin
 }
-
+ 
 // Gets the ADC value from analog pin channel
 unsigned int get_ADC(unsigned char channel)
 {
@@ -63,7 +45,7 @@ unsigned int get_ADC(unsigned char channel)
     while(ADCSRA & (1<<ADSC));       // While ADC is still in progress, do nothing
     return ADC;                      // Return the ADC value
 }
-
+ 
 // Is used to start the ADC conversion -> make parameters a reference to (&) ADCSRA, ADSC, and 1, respectively
 void change_bit(volatile uint8_t *SFR, unsigned char my_bit, unsigned char bit_val)
 {
