336a39e8ce225c0c434ddb21371402e1fd395586
Noam Schuck <noam.schuck@gmail.com>
2024-05-02 14:16:07 -0400

    add prototype functions of wheel control and PWM

--- main.c
+++ main.c
@@ -1,75 +1,67 @@
 // ATMEGA328P Datasheet: https://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-7810-Automotive-Microcontrollers-ATmega328P_Datasheet.pdf
 
 /* Pins:
- *      Port D    - Output
- *      Port D, 1 - Passed threshold indicator
- *      Port C, 0 - IR Sensor
- *      Port C, 1 - Potentiometer
+ *      Port D      - Output
+ *      Port D, 0   - Motor 1 Enable
+ *      Port D, 1-2 - Motor 1 Control
+ *      Port D, 3   - Motor 2 Enable
+ *      Port D, 4-5 - Motor 2 Control
+ *      Port C, 0   - IR Sensor
+ *      Port C, 1   - Potentiometer
+ *      Port B, 1   - PWM For Motor
  */
 
 #include <xc.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 
-void setup_ADC(void);
-void setup_interrupt(void);
-unsigned int get_ADC(unsigned char);
-void change_bit(volatile uint8_t *, unsigned char, unsigned char);
+#define SPEED_PIN 1
+#define M1E 0
+#define M1A 1
+#define M1B 2
+#define M2E 3
+#define M2A 4
+#define M2B 5
 
-unsigned int threshold = 0;
 
 int main(void)
 {
-    DDRD = 0b11111111;     // Sets all bits in Port D to be outputs
+    DDRD |= (1 << M1A) | (1 << M1B) | (1 << M2A) | (1 << M2B);
+    DDRB |= (1 << SPEED_PIN);  // Sets motor speed pin to be output
     
-    setup_ADC();
-    threshold = get_ADC(1); // Sets the treshold to the potentiometer value
-    
-    setup_interrupt();
-    
-    // What happens from now till infinity
+    setup_PWM();
+
     while(1) {
-        threshold = getADC(1);
+        OCR1A = 128; // Sets motor speed to half
     }
 }
 
-// Interupt service routine for timer 0 
-ISR(TIMER0_OVF_vect) 
-{
-    unsigned int result = get_ADC(0);                  // Get the result of the IR ADC
-    TCNT0 = 0;                                         // Reset the timer count to 0
-    PORTD &= (result >= threshold) ? !(1 << 7) : 0xFF; // Set PD1 to 1 if result >= threshold
+void setup_PWM(void) {
+    TCCR1B  = (1 << CS12) | (1 << CS10); // Sets Prescaler for timer 1 to 256
+    TCCR1A  = (1 << COM1A1) & ~(1 << COM1A0); // Defines that when a match occurrs to update the PWM when the timer goes to 0 again
+    TCCR1A |= (1 << WGM11) | (1 << WGM10); // Turns on fast PWM mode
 }
 
-void setup_interrupt(void) {
-    TCCR0B |= (1 << CS02);  // Select 1024 clock cycles for the interupt
-    TIMSK0 |= (1 << TOIE0); // Enables the timer interupt 
-    sei();                  // Enables general interupt
-    TCNT0 = 0;              // Sets the timer counter to 0 (i think)
-}
+/* Changes the direction of the robot
+ * Parameters:
+ *     duration     - how long to turn for in ms
+ *     direction    - determines motor operation given by motorcontroller truth table
+ *     synchronized - if 0 targets left motor, if 1 targets right motor, if 2 targets both
+*/
 
-// Sets up ADC
-void setup_ADC(void)
-{
-    ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0); // Enable ADC | Set Prescaler Bits (0-2) to 111 (128 ticks of system clock is 1 tick for the ADC (i think))
-    ADMUX = (1<<REFS0);                                  // Set voltage reference to AVcc with external cap at AREF pin
-}
-
-// Gets the ADC value from analog pin channel
-unsigned int get_ADC(unsigned char channel)
-{
-    ADMUX = (ADMUX & 0xF8)|channel;  // Do ADC on channel pin
-    change_bit(&ADCSRA, ADSC, 1);    // Start ADC conversion
-    while(ADCSRA & (1<<ADSC));       // While ADC is still in progress, do nothing
-    return ADC;                      // Return the ADC value
-}
-
-// Is used to start the ADC conversion -> make parameters a reference to (&) ADCSRA, ADSC, and 1, respectively
-void change_bit(volatile uint8_t *SFR, unsigned char my_bit, unsigned char bit_val)
-{
-    if(bit_val == 0) {
-        *SFR &= ~(1 << my_bit); // If my_bit is ADSC then it will stop ADC conversion
+void change_direction(unsigned int duration, unsigned char direction, unsigned char synchronized) {
+    if(synchronized == 2) {
+        PORTD &= ~(~direction << M1A) | ~(~direction << M2A); // AND it to get the 0s in place
+        PORTD |= (direction << M1A) | (direction << M2A);     // OR it to get the 1s in place
+        PORTD |= (1 << M1E) | (1 << M2E); // Enable
     } else {
-        *SFR |= (1 << my_bit); // If my_bit is ADSC then it will start ADC conversion
+        PORTD &= wheel ? ~(~direction << M1A) : ~(~direction << M1B); // Writes the 1s in the direction
+        PORTD |= wheel ? (direction << M1A) : (direction << M1B); // Writes the 0s in the direction
+        PORTD |= wheel ? (1 << M1E) : (1 << M2E); // Enables the turning
     }
+
+    _delay_ms(duration);
+    PORTD &= ~(1 << M1E) | ~(1 << M2E); // Enable
 }
+
